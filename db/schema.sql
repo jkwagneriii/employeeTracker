02549c50e5ab475176658bf6e136045d592301ce
Jake Wagner <jkwagneriii@gmail.com>
2020-10-27 09:52:12 -0500

    working on schema

--- db/schema.sql
+++ db/schema.sql
@@ -1,11 +1,38 @@
---EMPLOYEE DATABASE 
+DROP DATABASE IF EXISTS employee_db;
+
+-- EMPLOYEE DATABASE 
 CREATE DATABASE employee_db;
 
 USE employee_db;
 
-CREATE TABLE department ();
+CREATE TABLE department (
+    id INT AUTO_INCREMENT,
+    name VARCHAR(50) NOT NULL,
+    PRIMARY KEY (id)
+);
 
-CREATE TABLE jobRole ();
+CREATE TABLE jobRole (
+    id INT AUTO_INCREMENT,
+    title VARCHAR(30) NOT NULL, 
+    salary VARCHAR(100) NOT NULL, 
+    department_id INT NOT NULL, 
+    PRIMARY KEY(id),
+    FOREIGN KEY(department_id)
+    REFERENCES department(id)
+);
 
-CREATE TABLE employee ();
+CREATE TABLE employee (
+    id INT AUTO_INCREMENT, 
+    first_name VARCHAR(30) NOT NULL, 
+    last_name VARCHAR(30) NOT NULL, 
+    role_id INT NOT NULL, 
+    manager_id INT,
+    PRIMARY KEY(id),
+
+    FOREIGN KEY(role_id)
+    REFERENCES role(id),
+
+    FOREIGN KEY(manager_id)
+    REFERENCES employee(id)
+);
 
