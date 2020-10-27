80b7fbde18b08d7dd8b751757ab5fef17f389c44
Jake Wagner <jkwagneriii@gmail.com>
2020-10-27 16:47:07 -0500

    reinvestigated the READMEinstructions to correct my naming conventions.

--- db/schema.sql
+++ db/schema.sql
@@ -1,38 +1,32 @@
 DROP DATABASE IF EXISTS employee_db;
 
--- EMPLOYEE DATABASE 
 CREATE DATABASE employee_db;
 
 USE employee_db;
 
 CREATE TABLE department (
-    id INT AUTO_INCREMENT,
-    name VARCHAR(50) NOT NULL,
-    PRIMARY KEY (id)
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(30) NOT NULL
 );
 
-CREATE TABLE jobRole (
-    id INT AUTO_INCREMENT,
-    title VARCHAR(30) NOT NULL, 
-    salary VARCHAR(100) NOT NULL, 
-    department_id INT NOT NULL, 
-    PRIMARY KEY(id),
-    FOREIGN KEY(department_id)
-    REFERENCES department(id)
+CREATE TABLE role (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(30)NOT NULL,
+    salary DECIMAL (10,2) NOT NULL,
+    department_id INT NOT NULL,
+    FOREIGN KEY(department_id) REFERENCES department (id)
 );
 
 CREATE TABLE employee (
-    id INT AUTO_INCREMENT, 
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30) NOT NULL, 
-    last_name VARCHAR(30) NOT NULL, 
+    last_name VARCHAR(30) NOT NULL,
     role_id INT NOT NULL, 
-    manager_id INT,
-    PRIMARY KEY(id),
-
-    FOREIGN KEY(role_id)
-    REFERENCES role(id),
-
-    FOREIGN KEY(manager_id)
-    REFERENCES employee(id)
+    manager_id INT ,
+    FOREIGN KEY (manager_id) REFERENCES employee(id),
+    FOREIGN KEY(role_id) REFERENCES role(id)
 );
+
+SELECT * FROM department;
+
 
---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+USE employee_db;
+
+INSERT INTO department (name) VALUES ("Production");
+INSERT INTO department (name) VALUES ("Research and Development");
+INSERT INTO department (name) VALUES ("Marketing");
+INSERT INTO department (name) VALUES ("Accounting and Finance");
