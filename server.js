7ad199fc4815ab99c16d03860f9a021cd6c06783
Jake Wagner <jkwagneriii@gmail.com>
2020-10-27 17:13:09 -0500

    restarting server page, realized CLI won't involve express

--- server.js
+++ server.js
@@ -1,15 +1,6 @@
 //Dependencies 
-const express = require('express');
 const inquirer = require('inquirer');
+const mysql = require('mysql');
 
-//Routes 
-const apiRoutes = require('./routes/apiRoutes');
-const htmlRoutes = require('./routes/htmlRoutes');
 
-//Express 
-const app = express();
-const PORT = process.env.PORT || 3000;
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(express.static('public'));
-app.listen(PORT, () => console.log(`Listening on PORT: PORT`));
+
