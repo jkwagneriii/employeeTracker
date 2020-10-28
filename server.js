16b75e954c887bd305261a7bfcef7428d5d2b4e4
Jake Wagner <jkwagneriii@gmail.com>
2020-10-27 21:10:18 -0500

    still having a hard time getting the first prompt to connect back to the database. Spent time psuedo coding through the rest of the server page so that I have a solid base to start tomorrow and for my tutor sesh on thursday

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 //Dependencies 
-const inquirer = require('inquirer');
 const mysql = require('mysql');
+const inquirer = require('inquirer');
 
-var connection = mysql.createConnection({
+let connection = mysql.createConnection({
     host: "localhost",
 
     // Your port; if not 3306
@@ -35,12 +35,18 @@ function init() {
                 "View all employees",
                 "Exit"
             ]
-        }).then(function(response) {
+        })
+        .then(function(response) {
             //If Else statements for matching the response from the user to the query functions that link to the database.
-            if(response.command === "View all departments") {
-                console.log('DEPARTMENTS');
+            if (response.command === "View all departments") {
                 viewDepartments();
             }
+            // else if (response.command === "View all roles") {
+            //     viewRoles();
+            // }
+            // else if (response.command === "View all employees") {
+            //     viewEmployees();
+            // }
             else if (response.command === "Exit") {
                 connection.end();
             }
@@ -49,14 +55,48 @@ function init() {
 
 //Query functions here
 //NOT WORKING!
-// function viewDepartments() {
-//     let query = "SELECT * FROM department";
+function viewDepartments() {
+    let query = "SELECT * FROM department";
+    connection.query(query, function(err, res) {
+        console.log('DEPARTMENTS:')
+        res.forEach(function department() {
+            console.log(`ID: ${department.id}, Name: ${department.name}`)
+        })
+        init();
+    })
+};
+
+// function viewRoles() {
+//     let query = "SELECT * FROM role";
 //     connection.query(query, function(err, res) {
-//         console.log('DEPARTMENTS:')
-//         res.forEach(function department() {
-//             console.log(`ID: ${department.id}, Name: ${department.name}`)
+//         console.log('ROLES: ')
+//         res.forEach(function role() {
+//             console.log(`ID: ${role.id} | Title: ${role.title} | Salary: ${role.salary} | Department ID: ${role.department_id}`);
 //         })
 //         init();
-//     })
+//     });
 // };
 
+// function viewEmployees() {
+//     let query = "SELECT * FROM employee";
+//     connection.query(query, function(err, res) {
+//         console.log('EMPLOYEES: ')
+//         res.forEach(function employee() {
+//             console.log(`ID: ${employee.id} | Name: ${employee.first_name} ${employee.last_name} | Role ID: ${employee.role_id} | Manager ID: ${employee.manager_id}`);
+//         })
+//         init();
+//     });
+// };
+
+//I still need at least one adding function. Start with add department. Use inquirer to prompt another input for new department name. Then use an INSERT INTO ? to add the new data into the department table.
+
+// let query = "INSERT INTO department (name) VALUES (?)";
+// function addDepartment() {
+//     inquirer
+//         .prompt({
+//             type: 'input',
+//         })
+//         .then(function(response) {
+
+//         })
+// }
