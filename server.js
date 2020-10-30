5cd25a51213a433d7147e556fe87bb2a7eef7c5b
Jake Wagner <jkwagneriii@gmail.com>
2020-10-29 21:26:00 -0500

    cleaning up messages to users, organzing comments, and making some adjustments for a submission deadline with the anticipation of revisting the functionality later on

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 const mysql = require('mysql');
 const inquirer = require('inquirer');
 
+//CONNECTION TO MYSQL SERVER
 let connection = mysql.createConnection({
     host: "localhost",
 
@@ -18,13 +19,13 @@ let connection = mysql.createConnection({
 
   connection.connect(function(err) {
     if (err) throw err;
-    console.log("connected");
+    console.log("Connected to server");
     init();
   });
 
-//VIEW ALL DATA 
-function viewAll() {
-    let query = `SELECT employee.id, first_name, last_name, title, salary, departmentName, manager_id FROM employee JOIN roles on role_id = roles.id JOIN department ON department_id = department.id`
+//VIEW ALL DEPARTMENTS 
+function viewAllDepartments() {
+    let query = `SELECT departmentName, id FROM department`
     connection.query(query, function(err, result) {
         if (err) throw err;
         console.log("\n\n");
@@ -64,6 +65,7 @@ function viewEmployees() {
     console.clear();
 }
 
+//ADD DEPARTMENTS
 function addDepartment() {
     inquirer
         .prompt({
@@ -74,6 +76,7 @@ function addDepartment() {
         .then(function(response) {
             let query = `INSERT INTO department (departmentName) VALUES (?)`
             connection.query(query,[response.newDepartment],   function(err, result) {
+            if (err) throw err;    
             console.log("Department Successfully Added!");
             console.log("Press any arrow to return to CLI");
             })
@@ -82,6 +85,7 @@ function addDepartment() {
         })
 }
 
+//ADD ROLES 
 function addRoles() {
     let query = `SELECT * FROM department`
     connection.query(query, function(err, result) {
@@ -112,7 +116,6 @@ function addRoles() {
             }
         ])
         .then (function(response) {
-            console.log('HEY LOOk', response);
             let savedDepartmentId; 
             for (let i = 0; i < result.length; i++) {
                 if (result[i].departmentName === response.roleDepartment) {
@@ -122,52 +125,24 @@ function addRoles() {
 
             let query = `INSERT INTO roles (title, salary, department_id) VALUES (?, ?, ?)`
             connection.query(query, [response.roleTitle, response.roleSalary, savedDepartmentId], function(err, result) {
-                if (err) throw err;
+            if (err) throw err;
+            console.log("New Role Successfully Added!");
+            console.log("Press any arrow to return to CLI");
             })
             init();
+            console.clear();
         })
     })
-}
+};
 
-//NEW ARRAY HELPER
-function nameArrayHelper(array, key) {
-    let newArray = [];
-    for (let i = 0; i < array.length; i++) {
-        newArray.push(array[i][key]);
-    }
-
-    return newArray
-}
-
-//UPDATE ROLE ID
-function updateRoleID () {
-    let query = `SELECT * FROM roles`
-    connection.query(query, function(err, roleResults) {
-      
-        let query = `SELECT * FROM employee`
-
-        connection.query(query, function(err, empResults) {
-
-            let roleTitles = nameArrayHelper(roleResults, 'title');
-            let empNames = nameArrayHelper(empResults, 'first_name');
-            
-
-            console.log('titles and names ????', roleTitles, empNames)
-
-            // inquirer and connection query (figure out id's)
-            //Instead of INSERT INTO 
-            //UPDATE employee SET role_id = ? WHERE id = ? 
-            inquirer
-        })
-    })
-}
-
+//MAIN CLI FUNCTION 
 function init() {
     //Inquirer
+    console.clear();
     inquirer
         .prompt({
             name: "command",
-            type: "rawlist",
+            type: "list",
             message: "Hello... What would you like to do?",
             choices: [
                 "View all departments",
@@ -175,14 +150,14 @@ function init() {
                 "View all employees",
                 "Add new department",
                 "Add new role",
-                "Update employee",
+                // "Update employee",
                 "Exit"
             ]
         })
         .then(function(response) {
             //If Else statements for matching the response from the user to the query functions that link to the database.
             if (response.command === "View all departments") {
-                viewAll();
+                viewAllDepartments();
             }
             else if (response.command === "View all employees") {
                 viewEmployees();
@@ -195,11 +170,49 @@ function init() {
             }
             else if (response.command === "Add new role") {
                 addRoles();
-            } else if (response.command === "Update employee") {
-                updateRoleID()
-            }
+            } 
+            // else if (response.command === "Update employee") {
+            //     updateRoleID()
+            // }
             else if (response.command === "Exit") {
                 connection.end();
+                console.clear();
             }
-        })
+     })
 };
+
+
+// NEW ARRAY HELPER
+// function nameArrayHelper(array, key) {
+//     let newArray = [];
+//     for (let i = 0; i < array.length; i++) {
+//         newArray.push(array[i][key]);
+//     }
+
+//     return newArray
+// }
+
+//UPDATE ROLE ID
+// function updateRoleID () {
+//     let query = `SELECT * FROM roles`
+//     connection.query(query, function(err, roleResults) {
+      
+//         let query = `SELECT * FROM employee`
+
+//         connection.query(query, function(err, empResults) {
+
+//             let roleTitles = nameArrayHelper(roleResults, 'title');
+//             let empNames = nameArrayHelper(empResults, 'first_name');
+            
+
+//             console.log('titles and names ????', roleTitles, empNames)
+
+//             // inquirer and connection query (figure out id's)
+//             //Instead of INSERT INTO 
+//             //UPDATE employee SET role_id = ? WHERE id = ? 
+//             inquirer
+//         })
+//     })
+// }
+
+
